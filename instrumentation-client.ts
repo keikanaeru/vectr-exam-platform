0d6f6339ba248521bf4df7338adfc276b2435f23
keikanaeru <syihabrifqi2406@gmail.com>
2026-08-20 07:26:26 +0700

    Add Sentry monitoring

---
+++ instrumentation-client.ts
@@ -0,0 +1,25 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dd69e757e59f3a76301d609d1dbf5307@o4511940078141440.ingest.de.sentry.io/4511940085874768",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 0.05,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+  sendDefaultPii: false,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
+
---
+++ sentry.edge.config.ts
@@ -0,0 +1,25 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dd69e757e59f3a76301d609d1dbf5307@o4511940078141440.ingest.de.sentry.io/4511940085874768",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 0.05,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+  sendDefaultPii: false,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
+
